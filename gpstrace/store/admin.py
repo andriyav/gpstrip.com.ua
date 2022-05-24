99117f3da815ccec5370bb58bd7588271f45127b
andriyav@hotmail.com <andriyav@hotmail.com>
2022-05-24 22:34:47 +0300

    module Category

--- gpstrace/store/admin.py
+++ gpstrace/store/admin.py
@@ -3,4 +3,5 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Item)
+admin.site.register(Category)
 # Register your models here.
--- gpstrace/store/models.py
+++ gpstrace/store/models.py
@@ -12,6 +12,7 @@ class Item(models.Model):
     discount = models.DecimalField(max_digits=7, decimal_places=2, blank=True, null=True)
     description = models.TextField(max_length=1000, null=True, blank=True)
     label = models.CharField(choices=LABEL_CHOICES, max_length=20, null=True, blank=True)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True )
 
 
     def __str__(self):
@@ -20,6 +21,14 @@ class Item(models.Model):
     def discount_price_calculation(self):
         self.discount_price = self.price - (self.price * (self.discount/100))
         return self.discount_price
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+
+    def __str__(self):
+        return self.name
+
 
     # def get_top_sales(self):
     #     self.top_sales = Item.objects.filter(label='Популярне')
