5f0b01bcf440898a6a313b3506299f143d18171c
andriyav@hotmail.com <andriyav@hotmail.com>
2022-06-15 00:20:56 +0300

    sort by price

--- gpstrace/store/views.py
+++ gpstrace/store/views.py
@@ -25,7 +25,15 @@ class HomeView(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
+        if 'max' in self.request.GET and 'min' in self.request.GET:
+            price_max = self.request.GET['max']
+            price_min = self.request.GET['min']
+            price_filter = Item.objects.filter(price__lte=price_max, price__gte=price_min)
+        else:
+            price_filter = Item.objects.all()
         context['title'] = 'Інтернет магазин GPSTrace'
+        context['items'] = price_filter
+
         return context
 
 
@@ -60,3 +68,8 @@ class ShowCategory(ListView):
     model = Category
     template_name = 'store/store.html'
     context_object_name = 'cats'
+
+class BlankView(ListView):
+    model = Item
+    template_name = "store/blank.html"
+    context_object_name = 'blank'
