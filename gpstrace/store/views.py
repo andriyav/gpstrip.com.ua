761c35d586327b535edfbcbaeab5b9366425f571
andriyav@hotmail.com <andriyav@hotmail.com>
2022-07-02 00:09:31 +0300

    add_to_cart

--- gpstrace/store/views.py
+++ gpstrace/store/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.views import LoginView
 from django.contrib.auth import logout, login
-from .models import Item, Category
+from .models import Item, Category, OrderItem, Order
 from .forms import RegisterUserForm, LoginUserForm
 from django.urls import reverse_lazy
 
@@ -107,4 +107,26 @@ class LoginUser(LoginView):
 def logout_user(request):
     logout(request)
     return redirect('home')
+
+def add_to_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_item, created = OrderItem.objects.get_or_create(
+            item=item,
+            user=request.user,
+            ordered=False
+        )
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item.quantity += 1
+            order_item.save()
+        else:
+            order.items.add(order_item)
+    else:
+        ordered_date = timezone.now()
+        order = Order.objects.create(
+            user=request.user, ordered_date=ordered_date)
+        order.items.add(order_item)
+    return redirect("core:product", slug=slug)
 
