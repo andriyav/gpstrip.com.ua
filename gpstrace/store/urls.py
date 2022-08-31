b28e3e9bc5f384447626e9a0e3e3204423526c7e
andriyav@hotmail.com <andriyav@hotmail.com>
2022-08-31 22:35:29 +0300

    index_view

--- gpstrace/store/urls.py
+++ gpstrace/store/urls.py
@@ -6,11 +6,11 @@ from . views import *
 
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
+    path('', IndexView.as_view(), name='home'),
     path('item/<slug:item_slug>/', ShowItem.as_view(), name='itemv'),
     path('category/<slug:cat_slug>/', CategoryTracker.as_view(), name='category'),
     path('checkout', CheckOutView.as_view(), name='checkout'),
-    path('index', IndexView.as_view(), name='index'),
+    path('index', HomeView.as_view(), name='index'),
     path('cart', CartView.as_view(), name='cart'),
     path('register', RegisterUser.as_view(), name='register'),
     path('logout/', logout_user, name='logout'),
