2bc6eeff98c8a49df348b98fc015d9c4530266d8
andriyav@hotmail.com <andriyav@hotmail.com>
2022-11-25 00:45:47 +0200

    New user models 6 registration Form

--- gpstrace/account/views.py
+++ gpstrace/account/views.py
@@ -5,8 +5,10 @@ from django.http import HttpResponse
 from django.contrib.sites.shortcuts import get_current_site
 from .tokens import account_activation_token
 from django.utils.encoding import force_bytes
+from django.template.loader import render_to_string
 
 def account_register(request):
+
     if request.user.is_authenticated:
         return redirect('account:dashboard')
 
@@ -18,20 +20,19 @@ def account_register(request):
             user.set_password(registerForm.cleaned_data['password'])
             user.is_active = False
             user.save()
-            # Setup email
             current_site = get_current_site(request)
-            subject = "Активуйте свій обліковий запис"
-            message = render('account/registration/account_activaion_email.html', {
-                             'user': user,
-                            'domain': current_site.domian,
-                             'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                            'token': account_activation_token.make_token(user),
+            subject = 'Activate your Account'
+            message = render_to_string('account/registration/account_activation_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
             })
             user.email_user(subject=subject, message=message)
-            return HttpResponse('registered successfully and activation sent')
-        else:
-            registerForm = RegistrationForm()
-        return render(request, 'account/registration/register.html', {'form': registerForm})
+            return HttpResponse('registered succesfully and activation sent')
+    else:
+        registerForm = RegistrationForm()
+    return render(request, 'account/registration/register.html', {'form': registerForm})
     #
     # def account_activate(requiest, uidb64)
 
--- gpstrace/gpstrace/urls.py
+++ gpstrace/gpstrace/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('', include('store.urls')),
     path('cart/', include('cart.urls')),
     path('', include('cart.urls')),
-    path('', include('account.urls'))
+    path('account/', include('account.urls', namespace='account')),
 ]
 if settings.DEBUG:
 
