efbe8199c3badac66ecec0cec9f524c7d1ceff69
andriyav@hotmail.com <andriyav@hotmail.com>
2023-01-03 21:57:14 +0200

    newsletter app

--- gpstrace/store/views.py
+++ gpstrace/store/views.py
@@ -193,7 +193,9 @@ class Search(ListView):
     paginate_by = 3
 
     def get_queryset(self):
-        return Item.objects.filter(description__icontains=self.request.GET.get('drop', ''))
+
+        return Item.objects.filter(Q(description__icontains=self.request.GET.get('drop', '')) &
+                                   Q(description__icontains=self.request.GET.get('q', '')))
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
