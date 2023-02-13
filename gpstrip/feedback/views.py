f148d46957f5e4a78ac8e5dd002e3944a3f6e0b9
andriyav@hotmail.com <andriyav@hotmail.com>
2023-02-13 21:41:02 +0200

    feedback для товару, можливість відгуку тільки під замовленим товаром, вивід кількості відгуків

--- gpstrip/feedback/views.py
+++ gpstrip/feedback/views.py
@@ -1,20 +1,18 @@
-
 from django.shortcuts import render, redirect
 from . models import Feedback
 from store.models import Order, OrderItem, Item
 from django.utils import timezone
 
 def feedback_product(request):
-
     if request.method == 'POST':
-        item = Item.objects.filter(slug=request.POST['slug'])
-        order = OrderItem.objects.filter(user=request.user, item=item)
-        print(item.slug)
-        print(request.POST)
-        Feedback.objects.get_or_create(feedback_text=request.POST['feedback_text'],
-                                       user_feedback=request.user.first_name,
-                                       time_feedback=timezone.now(),
-                                       slug=request.POST['slug']
-                                    )
+        orders = OrderItem.objects.filter(user=request.user, ordered=True)
+        for order in orders:
+            if order.item.slug in request.POST['slug']:
+                Feedback.objects.get_or_create(feedback_text=request.POST['feedback_text'],
+                                               user_feedback=request.user.first_name,
+                                               time_feedback=timezone.now(),
+                                               slug=request.POST['slug']
+                                            )
+                break
     return redirect(request.META.get('HTTP_REFERER'))
 # Create your views here.
